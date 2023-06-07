2c9ebb2f22e79da3dd73fcfcfae9aa0034dd7f4a
gritchin-artur <123599698+gritchin-artur@users.noreply.github.com>
2023-06-07 12:59:14 +0200

    Update animal-api.js

--- src/animal-api.js
+++ src/animal-api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 export async function fetchGalaryByBree(topic, pages) {
-  MY_KEY = '36910570-35daf5d8a5ff9002bcd25fc68';
+  // MY_KEY = '36910570-35daf5d8a5ff9002bcd25fc68';
   URL = 'https://pixabay.com/api/';
    console.log(topic)
   console.log(pages)
